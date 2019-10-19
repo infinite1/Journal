cd40836e20be5d5ef7d029fbdabb3044ac72120f
camizous <camizous@gmail.com>
2019-10-19 16:16:13 +1100

    merged merge video

--- app/src/main/java/com/example/journal3/MainActivity.java
+++ app/src/main/java/com/example/journal3/MainActivity.java
@@ -1,39 +1,205 @@
 package com.example.journal3;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import com.google.android.material.navigation.NavigationView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar myToolbar;
-    private Button mSignIn;
+    private int REQUEST_TAKE_GALLERY_VIDEO1=100;
+    private int REQUEST_TAKE_GALLERY_VIDEO2=101;
+    private int PERMISSION_CODE=99;
+    Dialog demo_dialog;
+    Button mash;
+    private Uri videoUri = null;
+    private Uri videoUri2 = null;
+    private String videoUriPath;
+    private String videoUri2Path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.navigation_activity);
-        myToolbar = findViewById(R.id.back);
 
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        List<String> permissionList = new ArrayList<>();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        View headerview = navigationView.getHeaderView(0);
-        mSignIn = headerview.findViewById(R.id.nav_sign_in_btn);
-        mSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.CAMERA);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.INTERNET);
+        }
+
+        if (!permissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    permissionList.toArray(new String[permissionList.size()]), PERMISSION_CODE);
+        } else {
+            Toast.makeText(this, "ALL Permissions granted", Toast.LENGTH_LONG).show();
+        }
+
+
+
+        setContentView(R.layout.demo_window);
+        demo_dialog = new Dialog(this);
+
+        mash = findViewById(R.id.save_share);
+        mash.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, LogInActivity.class);
-                startActivity(i);
-            }
-        });
+                Intent intent = new Intent(MainActivity.this, SaveShare.class);
+                Log.v("TEST:",videoUriPath);
+                Log.v("TEST:",videoUri2Path);
+                intent.putExtra("videoUri",videoUriPath);
+                intent.putExtra("videoUri2",videoUri2Path);
 
-
+                startActivity(intent);
+                }
+            });
     }
+
+
+        public void ShowPopup_order(View v) {
+            TextView txtclose;
+            demo_dialog.setContentView(R.layout.pop_up);
+            txtclose = (TextView) demo_dialog.findViewById(R.id.txtclose);
+            txtclose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    demo_dialog.dismiss();
+                }
+            });
+            demo_dialog.show();
+        }
+
+        public void ShowPopup_music(View v) {
+            TextView txtclose;
+            demo_dialog.setContentView(R.layout.pop_up_music);
+            txtclose = (TextView) demo_dialog.findViewById(R.id.txtclose);
+            txtclose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    demo_dialog.dismiss();
+                }
+            });
+            demo_dialog.show();
+        }
+
+        public void ShowPopup_calendar(View v) {
+            TextView txtclose;
+            demo_dialog.setContentView(R.layout.pop_up_calendar);
+            txtclose = (TextView) demo_dialog.findViewById(R.id.txtclose);
+            txtclose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    demo_dialog.dismiss();
+                }
+            });
+            demo_dialog.show();
+        }
+
+        public void save_share(View v)
+        {
+            TextView txtclose;
+            demo_dialog.setContentView(R.layout.pop_up_calendar);
+            txtclose=(TextView)demo_dialog.findViewById(R.id.txtclose);
+            txtclose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    demo_dialog.dismiss();
+                }
+            });
+            demo_dialog.show();
+        }
+
+        public void upload_video(View v) {
+            TextView txtclose;
+            demo_dialog.setContentView(R.layout.pop_upload_video);
+            txtclose = (TextView) demo_dialog.findViewById(R.id.txtclose);
+            txtclose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    demo_dialog.dismiss();
+                }
+            });
+            demo_dialog.show();
+        }
+
+        public void video1(View v) {
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+            if(intent.resolveActivity(getPackageManager()) != null )
+            {
+                startActivityForResult(intent,REQUEST_TAKE_GALLERY_VIDEO1);
+            }
+        }
+
+        public void video2(View v) {
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+            if(intent.resolveActivity(getPackageManager()) != null )
+            {
+                startActivityForResult(intent,REQUEST_TAKE_GALLERY_VIDEO2);
+            }
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == REQUEST_TAKE_GALLERY_VIDEO1 && resultCode == RESULT_OK) {
+                videoUri = data.getData();
+                videoUriPath = convertMediaUriToPath(videoUri);
+
+            }
+            if (requestCode == REQUEST_TAKE_GALLERY_VIDEO2 && resultCode == RESULT_OK) {
+                videoUri2 = data.getData();
+                videoUri2Path = convertMediaUriToPath(videoUri2);
+
+
+            }
+    }
+
+    public String convertMediaUriToPath(Uri uri) {
+        String [] proj={MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, proj,  null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String path = cursor.getString(column_index);
+        cursor.close();
+        return path;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/journal3/SaveShare.java
@@ -0,0 +1,137 @@
+package com.example.journal3;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SaveShare extends AppCompatActivity {
+
+    private String workingPath;//输出文件目录
+    private ArrayList<String> videosToMerge = new ArrayList<>();//需要合并的视频的路径集合
+    private String outName = "mergeVideo.mp4";//输出的视频名称
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.share_save);
+
+        Uri videoUri = Uri.parse(getIntent().getExtras().getString("videoUri"));
+        Uri videoUri2 = Uri.parse(getIntent().getExtras().getString("videoUri2"));
+        String content1 = getIntent().getExtras().getString("videoUri");
+        String content2 = getIntent().getExtras().getString("videoUri2");
+
+
+        videosToMerge.add(content1);
+        videosToMerge.add(content2);
+
+        Log.v("Test",content1);
+        Log.v("Test",content2);
+
+
+        String DCIMPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/output";
+//        workingPath = path;
+
+
+//        workingPath = path;
+        int count = videosToMerge.size();
+        try {
+            Movie[] inMovies = new Movie[count];
+            for (int i = 0; i < count; i++) {
+                inMovies[i] = MovieCreator.build(videosToMerge.get(i));
+            }
+
+            List<Track> videoTracks = new LinkedList<>();
+            List<Track> audioTracks = new LinkedList<>();
+
+            //提取所有视频和音频的通道
+            for (Movie m : inMovies) {
+                for (Track t : m.getTracks()) {
+                    if (t.getHandler().equals("soun")) {
+                        audioTracks.add(t);
+                    }
+                    if (t.getHandler().equals("vide")) {
+                        videoTracks.add(t);
+                    }
+                    if (t.getHandler().equals("")) {
+
+                    }
+                }
+            }
+
+            //添加通道到新的视频里
+            Movie result = new Movie();
+            if (audioTracks.size() > 0) {
+                result.addTrack(new AppendTrack(audioTracks
+                        .toArray(new Track[audioTracks.size()])));
+            }
+            if (videoTracks.size() > 0) {
+                result.addTrack(new AppendTrack(videoTracks
+                        .toArray(new Track[videoTracks.size()])));
+            }
+            Container mp4file = new DefaultMp4Builder()
+                    .build(result);
+
+
+            //开始生产mp4文件
+//            File storagePath = new File(workingPath);
+//            storagePath.mkdirs();
+            FileOutputStream fos = new FileOutputStream(new File("/storage/emulated/0/DCIM/Camera/out1.mp4"));
+            FileChannel fco = fos.getChannel();
+            mp4file.writeContainer(fco);
+            fco.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.v("TEST","video aleady producted");
+        File mergefile = new File("/storage/emulated/0/DCIM/Camera/out1.mp4");//打开软件直接播放的视频名字是movie.mp4
+        mVideoView = findViewById(R.id.merge_video);
+        Log.v("test", mergefile.getPath());
+        mVideoView.setVideoPath(mergefile.getPath());
+        mVideoView.seekTo(0);
+        mVideoView.requestFocus();
+        mVideoView.start();
+
+
+
+    }
+
+    public String convertMediaUriToPath(Uri uri) {
+        String [] proj={MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, proj,  null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String path = cursor.getString(column_index);
+        cursor.close();
+        return path;
+    }
+}
