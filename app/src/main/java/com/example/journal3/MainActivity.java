2f760407aa9812e5d34d03c7b3fb7d7f987a6ddc
lipeiyang <lipeiyanglpy@gmail.com>
2019-10-21 19:18:15 +1100

    add location

--- app/src/main/java/com/example/journal3/MainActivity.java
+++ app/src/main/java/com/example/journal3/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.journal3;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
+
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +40,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -37,22 +52,30 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.google.type.LatLng;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Logger;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 
 public class MainActivity extends AppCompatActivity implements
         BottomNavigationView.OnNavigationItemSelectedListener {
     private Toolbar myToolbar;
     private Button mSignIn;
     private BottomNavigationView mBtmView;
-   //add
+
+    //add
     private Uri videouri;
     private static final int REQUEST_CODE = 101;
     private ProgressDialog progressDialog;
@@ -69,6 +92,12 @@ public class MainActivity extends AppCompatActivity implements
     private TextView emailView;
     private FirebaseFirestore db;
     private Uri downloadUri;
+    //location
+    private LocationManager lm;
+    LocationListener locationListener;
+    String currentlocation;
+
+
 
 
     public static final int MAX_SIZE = 100;
@@ -78,11 +107,11 @@ public class MainActivity extends AppCompatActivity implements
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_activity);
-
         List<String> permissionList = new ArrayList<>();
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
@@ -187,7 +216,6 @@ public class MainActivity extends AppCompatActivity implements
 
     }
 
-
     // Add select item listener for bottomNavigation
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -210,6 +238,7 @@ public class MainActivity extends AppCompatActivity implements
         return true;
     }
 
+
     //add
 
     public void upload(View view) {
@@ -229,36 +258,40 @@ public class MainActivity extends AppCompatActivity implements
 
 
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.setTitle("Uploading file..");
-        progressDialog.setProgress(0);
-        progressDialog.show();
-        if (videouri != null) {
-            UploadTask uploadTask = videoref.putFile(videouri);
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setTitle("Uploading file..");
+            progressDialog.setProgress(0);
+            progressDialog.show();
+            if (videouri != null) {
+                UploadTask uploadTask = videoref.putFile(videouri);
 
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(MainActivity.this,
-                            "Upload failed: " + e.getLocalizedMessage(),
-                            Toast.LENGTH_LONG).show();
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,
+                                "Upload failed: " + e.getLocalizedMessage(),
+                                Toast.LENGTH_LONG).show();
 
-                }
-            }).addOnSuccessListener(
-                    new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Toast.makeText(MainActivity.this, "Upload complete",
-                                    Toast.LENGTH_LONG).show();
-                            progressDialog.dismiss();
-                            recordList.add(strDate);
+                    }
+                }).addOnSuccessListener(
+                        new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Toast.makeText(MainActivity.this, "Upload complete",
+                                        Toast.LENGTH_LONG).show();
+                                progressDialog.dismiss();
+                                recordList.add(strDate);
 //                            System.out.println("list size is "+list.size());
-                        }
-                    }).addOnProgressListener(
-                    new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                /******************************get location*************************/
+                                checkGPSSettings();
+                                System.out.println("currentLocation is " +currentlocation);
+
+                            }
+                        }).addOnProgressListener(
+                        new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
 //                            updateProgress(taskSnapshot);
 
                                 int currentProgress = (int) (100 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
@@ -273,10 +306,14 @@ public class MainActivity extends AppCompatActivity implements
                                     public void onSuccess(Uri uri) {
                                         downloadUri = uri;
                                         uploadRefToDatabase(currentUser, strDate);
+//                                        /******************************get location*************************/
+//                                        checkGPSSettings();
+                                        System.out.println("1111111currentLocation is " +currentlocation);
+
                                         /******************************add metadata*************************/
                                         StorageMetadata metadata = new StorageMetadata.Builder()
                                                 .setContentType("video/mp4")
-                                                .setCustomMetadata("Location", "Carlton")
+                                                .setCustomMetadata("Location", currentlocation)
                                                 .build();
                                         // Update metadata properties
                                         videoref.updateMetadata(metadata)
@@ -357,6 +394,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
 
+
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode, Intent data) {
@@ -369,11 +407,259 @@ public class MainActivity extends AppCompatActivity implements
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Video recording cancelled.",
                         Toast.LENGTH_LONG).show();
-            } else {
+            }else if(requestCode ==2) {
+                checkGPSSettings();
+                return;
+            }
+            else {
                 Toast.makeText(this, "Failed to record video",
                         Toast.LENGTH_LONG).show();
             }
         }
+    }
+    public void checkGPSSettings() {
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener()
+        {
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                //location.getProvider();
+                Log.d("haha", "" + location.getProvider() + " Location latitude " + latitude + "\nlongitude:" + longitude);
+//                currentlocation = latitude +","+longitude;
+
+                try {
+                    currentlocation = getLocation(latitude,longitude);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+
+
+        // Location hardware setting enabled?
+        boolean GPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        String[] permissionsArray = {
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+        };
+
+        if (GPSEnabled) {
+            // Android 6.0+
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (!checkPermissions(this, permissionsArray)) {
+                    // request code 1
+
+                    Log.d("haha", "request");
+
+                    ActivityCompat.requestPermissions(this, permissionsArray,
+                            1);
+                } else {
+                    // Permission has already been granted
+                    Log.d("haha", "line 52");
+                    startLocalisation();
+                }
+
+
+            } else {
+                // no runtime check
+                Log.d("haha", "line 74");
+                startLocalisation();
+            }
+        } else {
+            Log.d("haha", "line 82");
+            Toast.makeText(this, "GPS Not Enabled", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            // request code 2
+            startActivityForResult(intent, 2);
+        }
+    }
+
+
+
+
+
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+
+    }
+
+    public static boolean checkPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void startLocalisation() {
+
+        // parameters of location service
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+        // localisation uses a lot of power, consider your task cycle
+
+        //String provider = lm.getBestProvider(criteria, true);
+
+        // can also use a specific provider
+
+        // cellular or WIFI network can localise me
+        String providerNET = LocationManager.NETWORK_PROVIDER;
+
+        // gps signal often naive
+        String providerGPS = LocationManager.GPS_PROVIDER;
+
+
+        // must call this before using getLastKnownLocation
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
+            return;
+        }
+
+
+
+        boolean gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location net_loc = null, gps_loc = null, finalLoc = null;
+
+        if (gps_enabled) {
+            Log.d("haha", " gps_enabled");
+
+            //requestLocationUpdates(String provider, long minTime, float minDistance, LocationListener listener)
+
+            lm.requestLocationUpdates(providerGPS, 0, 0, locationListener);
+            gps_loc = lm.getLastKnownLocation(providerGPS);
+        }
+        if (network_enabled){
+            Log.d("haha", " net_enabled");
+            lm.requestLocationUpdates(providerNET, 0, 0, locationListener);
+            net_loc = lm.getLastKnownLocation(providerNET);
+        }
+
+        if (gps_loc != null && net_loc != null) {
+
+            Log.d("haha", "both available location");
+
+            //smaller the number more accurate result will
+            if (gps_loc.getAccuracy() > net_loc.getAccuracy())
+                finalLoc = net_loc;
+            else
+                finalLoc = gps_loc;
+
+            // I used this just to get an idea (if both avail, its upto you which you want to take as I've taken location with more accuracy)
+
+        } else {
+
+            if (gps_loc != null) {
+                finalLoc = gps_loc;
+                Log.d("haha", "gps available location");
+            } else if (net_loc != null) {
+                finalLoc = net_loc;
+                Log.d("haha", "net available location");
+            }
+        }
+//        if (gps_loc != null) {
+//                finalLoc = gps_loc;}
+
+        if (finalLoc != null) {
+
+            double latitude = finalLoc.getLatitude();
+
+            double longitude = finalLoc.getLongitude();
+
+            Log.d("haha", "latitude：" + latitude + "\nlongitude" + longitude);
+
+            // if we are in melbourne, we get negative latitude.
+            // it means south part of the earth.
+
+        } else {
+            Log.d("haha", "no available location");
+
+
+            //startLocalisation();
+        }
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            // request code 1
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.d("haha", "line 90");
+                    startLocalisation();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            default:
+                break;
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    public String getLocation(double latitude,double longtitude) throws IOException {
+        String city = "";
+        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+        try {
+            List<Address> addresses= geocoder.getFromLocation(latitude,longtitude,1);
+            String address = addresses.get(0).getAddressLine(0);
+            city = addresses.get(0).getLocality();
+            Log.d("address","Complets Address: "+ addresses.toString());
+            Log.d("address","Address: "+ address);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return city;
     }
 
 
